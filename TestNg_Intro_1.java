97dc4db5d60d24d2f435c97ef87d3b055d0789d9
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2025-05-29 16:36:23 +0530

    Add files via upload

---
+++ TestNg_Intro_1.java
@@ -0,0 +1,19 @@
+package TestNg;
+
+import org.testng.annotations.Test;
+
+public class TestNg_Intro_1 
+{
+	@Test
+	  public void testOne()
+	  {
+		System.out.println("This is the testing of the TestNbg");
+	  }
+	
+	@Test
+	public void testTwo()
+	{
+		System.out.println("This is the 2nd part of the Testing of TestNg");
+	}
+
+}
