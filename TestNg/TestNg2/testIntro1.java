fa8636888ddc3904378b1869e6ba713710552ab0
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2025-06-18 06:29:19 +0530

    Add files via upload

---
+++ TestNg/TestNg2/testIntro1.java
@@ -0,0 +1,21 @@
+package TestNg2;
+
+import org.testng.annotations.Test;
+
+public class testIntro1 
+{
+	
+	@Test
+	  public static void fun1()
+	  {
+		System.out.println("this is the FUN 1");
+	  }
+	
+	@Test
+	   public static void fun2()
+	   {
+		System.out.println("This is the FUn 2");
+	   }
+	
+
+}
---
+++ TestNg/TestNg2/testIntro2.java
@@ -0,0 +1,22 @@
+package TestNg2;
+
+import org.testng.annotations.Test;
+
+public class testIntro2 
+{
+	@Test
+	
+	  public static void CallIntro()
+	  {
+		System.out.println("This is the Call Intro Mthod");
+	  }
+	
+	@Test
+	    public static void CallControl()
+	    {
+		   System.out.println("This is the Call COntrol");
+	    }
+		
+	
+
+}
