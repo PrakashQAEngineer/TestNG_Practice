c9b125ca779963aaf920285a0937d68f619a3ca6
Prakash Singh Rajput <35395076+PrakashQAEngineer@users.noreply.github.com>
2026-02-17 07:46:41 +0530

    Add files via upload

---
+++ Wedding_Testng/src/testng1/Test1.java
@@ -0,0 +1,19 @@
+package testng1;
+
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+	@Test
+	public void testData()
+	{
+		System.out.println("This is my first TestNg");
+	}
+	
+	@Test
+	  public void testData2()
+	  {
+		System.out.println("This is the 2nd test");
+	  }
+
+}
---
+++ Wedding_Testng/src/testng1/Test2.java
@@ -0,0 +1,13 @@
+package testng1;
+
+import org.testng.annotations.Test;
+
+public class Test2 
+{
+	@Test
+	  public void test3()
+	  {
+		System.out.println("This is the test3");
+	  }
+
+}
